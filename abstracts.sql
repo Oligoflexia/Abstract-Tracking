8dcd24a4aa01563c7c00e62fba79decabbd8d961
Souvik Maiti <souvik.maiti1@ucalgary.ca>
2023-11-28 10:53:10 -0700

    added index and not null constraints to schema

--- abstracts.sql
+++ abstracts.sql
@@ -1,30 +1,29 @@
 CREATE TABLE People (
     PersonID INTEGER PRIMARY KEY,
     prefix TEXT,
-    first_name TEXT,
+    first_name TEXT NOT NULL,
     middle_name TEXT,
     last_name TEXT,
     post_nominals TEXT,
-    role TEXT
+    role TEXT,
 );
 
 CREATE TABLE Abstracts (
     AbstractID INTEGER PRIMARY KEY,
-    title TEXT,
-    authors BLOB, -- Just for human reference
+    title TEXT NOT NULL,
+    authors TEXT, -- Just for human reference
     summary BLOB,
     pop_size INTEGER,
     year INTEGER,
-    FOREIGN KEY(first_author) REFERENCES People(PersonID)
 );
 
 CREATE TABLE Conferences (
     ConferenceID INTEGER PRIMARY KEY,
-    conference_name TEXT,
+    conference_name TEXT NOT NULL,
     location TEXT,
     start_date TEXT, -- YYYY-MM-DD
     end_date TEXT, -- YYYY-MM-DD
-    organization TEXT
+    organization TEXT,
 );
 
 CREATE TABLE Attendances (
@@ -33,7 +32,7 @@ CREATE TABLE Attendances (
     role TEXT,
     PRIMARY KEY (ConferenceID, PersonID),
     FOREIGN KEY(PersonID) REFERENCES People(PersonID),
-    FOREIGN KEY(ConferenceID) REFERENCES Conferences(ConferenceID)
+    FOREIGN KEY(ConferenceID) REFERENCES Conferences(ConferenceID),
 );
 
 CREATE TABLE Authorship (
@@ -42,7 +41,7 @@ CREATE TABLE Authorship (
     role TEXT CHECK (status IN ('First Author', 'Co-First Author', 'Co-Author', 'Lab PI')),
     PRIMARY KEY (PersonID, AbstractID),
     FOREIGN KEY(PersonID) REFERENCES People(PersonID),
-    FOREIGN KEY(AbstractID) REFERENCES Abstracts(AbstractID)
+    FOREIGN KEY(AbstractID) REFERENCES Abstracts(AbstractID),
 );
 
 CREATE TABLE Presentations (
@@ -54,18 +53,39 @@ CREATE TABLE Presentations (
     PRIMARY KEY (ConferenceID, AbstractID, PersonID),
     FOREIGN KEY(ConferenceID) REFERENCES Conferences(ConferenceID),
     FOREIGN KEY(AbstractID) REFERENCES Abstracts(AbstractID),
-    FOREIGN KEY(PersonID) REFERENCES People(PersonID)
+    FOREIGN KEY(PersonID) REFERENCES People(PersonID),
 );
 
 CREATE TABLE Submissions (
+    SubmissionID INTEGER PRIMARY KEY,
     internal_ID TEXT,
     ConferenceID INTEGER,
     AbstractID INTEGER,
     SubmitterID INTEGER,
     section TEXT,
-    status TEXT CHECK (status IN ('Submitted', 'Draft', 'Draft Collection', 'Draft Analysis')),
+    status TEXT CHECK (status IN ('Submitted', 'Draft', 'Draft Collection', 'Draft Analysis')) NOT NULL,
     result TEXT,
     FOREIGN KEY(ConferenceID) REFERENCES Conferences(ConferenceID),
     FOREIGN KEY(AbstractID) REFERENCES Abstracts(AbstractID),
     FOREIGN KEY(SubmitterID) REFERENCES People(PersonID),
 );
+
+-- People
+CREATE INDEX fname_idx ON People (first_name);
+
+-- Abstracts
+
+-- Conferences
+CREATE INDEX confname_idx ON Conferences (conference_name);
+
+-- Attendances
+CREATE INDEX attd_idx ON Attendance (ConferenceID, PersonID);
+
+-- Authorship
+CREATE INDEX author_idx ON Authorship (PersonID, AbstractID);
+
+-- Presentations
+CREATE INDEX pres_idx ON Presentations (ConferenceID, PersonID);
+
+-- Submissions
+CREATE INDEX subm_idx ON Submissions (result, ConferenceID);
