a5d7d287131d8f65e2b93afa17947e364ed939ae
Souvik Maiti <souvik.maiti1@ucalgary.ca>
2023-11-30 13:52:22 -0700

    formatted to be PEP 8 compliant

--- app/interface/add_conf.py
+++ app/interface/add_conf.py
@@ -7,26 +7,30 @@ import logging
 
 from utils import db_operations, dbconnections
 
+
 class LoggingHandler(logging.Handler):
     def emit(self, record: LogRecord) -> None:
         log_entry = self.format(record)
         error_box(log_entry)
 
-def error_box(message:str):
+
+def error_box(message: str):
     messagebox.showerror("Error!", message)
 
-def prompt_box(name:str):
+
+def prompt_box(name: str):
     string = f"Are you sure you want to link new CSV records with: \n {name}"
     response = messagebox.askyesno("Confirm Selection", string)
     return response
 
+
 def submit():
-    
+
     name = name_entry.get()
-    if name == "": 
+    if name == "":
         error_box("Conference must have a name!")
         return
-    
+
     location = location_entry.get()
     organization = organization_entry.get()
     start_date = start_date_entry.get_date()
@@ -34,18 +38,22 @@ def submit():
     if start_date > end_date:
         error_box("Start date can't be after end date!")
         return
-    
+
     dict_list = [{"conference_name": name,
-                      "location": location,
-                      "start_date": start_date.strftime(r"%Y-%m-%d"),
-                      "end_date": end_date.strftime(r"%Y-%m-%d"),
-                      "organization": organization
-                    }
-                ]
-    
-    try: db_operations.add_conference_records(conn, dict_list)
-    except Exception as e: return
+                  "location": location,
+                  "start_date": start_date.strftime(r"%Y-%m-%d"),
+                  "end_date": end_date.strftime(r"%Y-%m-%d"),
+                  "organization": organization
+                  }
+                 ]
+
+    try:
+        db_operations.add_conference_records(conn, dict_list)
+    except Exception:
+        return
+
     get_conferences()
+
 
 def toggle_fields():
     if input_frame.winfo_viewable():
@@ -55,6 +63,7 @@ def toggle_fields():
         input_frame.grid()
         toggle_button.configure(text="Hide")
 
+
 def get_conferences():
     try:
         cursor.execute("SELECT * from Conferences")
@@ -63,10 +72,12 @@ def get_conferences():
     except sqlite3.Error as error:
         messagebox.showerror("Query Error", f"An error occurred: {error}")
 
-def update_table(rows):
+
+def update_table(rows: list[str]):
     tree.delete(*tree.get_children())
     for row in rows:
         tree.insert("", 'end', values=row)
+
 
 def on_treeview_click(event) -> int:
     region = tree.identify("region", event.x, event.y)
@@ -74,10 +85,11 @@ def on_treeview_click(event) -> int:
         row_id = tree.identify_row(event.y)
         row_item = tree.item(row_id)
         ConferenceID = row_item['values'][0]
-        
+
         if prompt_box(row_item['values'][1]):
             root.quit()
             return ConferenceID
+
 
 # Main window and frame setup
 root = tk.Tk()
@@ -92,11 +104,17 @@ root.rowconfigure(4, weight=0)
 root.rowconfigure(5, weight=0)
 root.rowconfigure(6, weight=1)
 
-ttk.Label(root, text="INFO:", font=("Arial", 10, "bold")).grid(row=0, sticky='w', padx=(10, 0), pady=(10, 0))
+ttk.Label(root,
+          text="INFO:",
+          font=("Arial", 10, "bold")
+          ).grid(row=0, sticky='w', padx=(10, 0), pady=(10, 0))
 
 string = "Click existing Conference record to link new CSV records."
 ttk.Label(root, text=string).grid(row=1, sticky='w', padx=(10, 0))
-ttk.Label(root, text="Add new Conference record:", font=("Arial", 10, "bold")).grid(row=2, sticky='w', padx=(10, 0), pady=(25, 0))
+ttk.Label(root,
+          text="Add new Conference record:",
+          font=("Arial", 10, "bold")
+          ).grid(row=2, sticky='w', padx=(10, 0), pady=(25, 0))
 
 toggle_button = ttk.Button(root, text="Show", command=toggle_fields)
 toggle_button.grid(row=3, column=0, sticky='w', padx=(10, 0), pady=(5, 0))
@@ -105,9 +123,12 @@ input_frame = ttk.Frame(root)
 input_frame.grid(row=4, sticky='ew', padx=(50, 0), pady=(10, 0))
 input_frame.grid_remove()
 
-ttk.Label(root, text="Current Conference records:", font=("Arial", 10, "bold")).grid(row=5, sticky='w', padx=(10, 0), pady=(30, 10))
+ttk.Label(root,
+          text="Current Conference records:",
+          font=("Arial", 10, "bold")
+          ).grid(row=5, sticky='w', padx=(10, 0), pady=(30, 10))
 
-#set up Conference records table
+# set up Conference records table
 tree = ttk.Treeview(root, columns=(1, 2, 3, 4, 5, 6), show="headings")
 tree.grid(row=6, sticky='nesw', padx=10, pady=(0, 10))
 tree.heading(1, text="ID")
@@ -126,49 +147,66 @@ tree.bind("<Button-1>", on_treeview_click)
 
 # Setup and place entry fields into frame
 # Name
-ttk.Label(input_frame, text="Conference Name:").grid(row=0, column=0, sticky='w', pady=5)
+ttk.Label(input_frame,
+          text="Conference Name:"
+          ).grid(row=0, column=0, sticky='w', pady=5)
 name_entry = ttk.Entry(input_frame)
 name_entry.grid(row=0, column=1, sticky='w', padx=5)
 
 # Location
-ttk.Label(input_frame, text="Location (City):").grid(row=1, column=0, sticky='w', pady=5)
+ttk.Label(input_frame,
+          text="Location (City):"
+          ).grid(row=1, column=0, sticky='w', pady=5)
 location_entry = ttk.Entry(input_frame)
 location_entry.grid(row=1, column=1, sticky='w', padx=5)
 
 # Organizer name
-ttk.Label(input_frame, text="Organizer Name:").grid(row=2, column=0, sticky='w', pady=5)
+ttk.Label(input_frame,
+          text="Organizer Name:"
+          ).grid(row=2, column=0, sticky='w', pady=5)
 organization_entry = ttk.Entry(input_frame)
 organization_entry.grid(row=2, column=1, sticky='w', padx=5)
 
 # start
-ttk.Label(input_frame, text="Start date:").grid(row=3, column=0, sticky='w', pady=2)
+ttk.Label(input_frame,
+          text="Start date:"
+          ).grid(row=3, column=0, sticky='w', pady=2)
 start_date_entry = DateEntry(input_frame, width=12)
 start_date_entry.grid(row=4, column=0, sticky='w', padx=3)
 
 # end
-ttk.Label(input_frame, text="End date:").grid(row=3, column=1, sticky='w', pady=2)
+ttk.Label(input_frame,
+          text="End date:"
+          ).grid(row=3, column=1, sticky='w', pady=2)
 end_date_entry = DateEntry(input_frame, width=12)
-end_date_entry.grid(row=4, column=1, sticky ='w', padx=3)
+end_date_entry.grid(row=4, column=1, sticky='w', padx=3)
 
 # buttons
 submit_button = ttk.Button(input_frame, text="Create Record", command=submit)
-submit_button.grid(row=5, column=0, columnspan=2, sticky='ew', padx=10, pady=(35,0))
+submit_button.grid(row=5,
+                   column=0,
+                   columnspan=2,
+                   sticky='ew',
+                   padx=10,
+                   pady=(35, 0)
+                   )
 
 if __name__ == "__main__":
-    
+
     dbconnection = dbconnections.DBConnection('abstracts.db')
-    
+
     try:
         logger = logging.getLogger()
         logger.addHandler(LoggingHandler())
-        
+
         conn = dbconnection.get_connection()
         cursor = conn.cursor()
 
         get_conferences()
-        
+
         root.lift()  # Bring the window to the top
-        root.after(1000, lambda: root.attributes("-topmost", False))  # Remove 'always on top' after 1 second
+        # Remove 'always on top' after 1 second
+        root.after(1000, lambda: root.attributes("-topmost", False))
         root.mainloop()
 
     finally:
